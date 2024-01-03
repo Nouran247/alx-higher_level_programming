155f45fa0a7c50872be9be1d59a1a3c26dac90b9
Nouran247 <noura2004chan@gmail.com>
2024-01-03 12:07:14 -0800

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for i in range(0, 10):
     for j in range (i+1, 10):
         print("{:d}{:d}".format(i, j),end="") 
         if(i+j != 17): print(",", end=" ") 
-        else: print(" ")
+        else: print("")
