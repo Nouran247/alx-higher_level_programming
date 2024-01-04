a13e7c71367bc6a3de2f6a815a13f2341fc03f00
Nouran247 <noura2004chan@gmail.com>
2024-01-04 10:22:02 -0800

    Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys
     
     lenth = len(sys.argv) - 1
-    if (lenth == 0):
+    if lenth == 0:
         print("0 arguments.")
-    elif (lenth == 1):
+    elif lenth == 1:
         print("1 argument:")
-    else
+    else:
         print("{} arguments:".format(lenth))
     for i in range(lenth):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
