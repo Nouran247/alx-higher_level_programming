4f1a17418b9d6129c94a0251477ae53b32dd08c6
Nouran247 <noura2004chan@gmail.com>
2024-01-08 14:46:23 -0800

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for i in my_string:
-        if i == 'c' and i == 'C':
-            continue
-        else:
+        if i != 'c' and i != 'C':
             new_string += i
-        return new_string
+    return new_string
