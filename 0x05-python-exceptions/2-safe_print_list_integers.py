6a16943a4e3b52d90d2914fbe5a6411a05758307
Nouran247 <noura2004chan@gmail.com>
2024-01-23 06:46:02 -0800

    Python - Exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    ctr = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ctr += 1
+        except (TypeError, ValueError):
+            pass
+        i += 1
+    print()
+    return c
